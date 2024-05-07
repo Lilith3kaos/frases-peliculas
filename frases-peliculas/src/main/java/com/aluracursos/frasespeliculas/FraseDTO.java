aac7c9ed92ed0943def3b2922e24a0a182936ba8
Lilith3kaos <131048274+Lilith3kaos@users.noreply.github.com>
2024-05-07 14:06:55 -0600

    Add files via upload

---
+++ frases-peliculas/src/main/java/com/aluracursos/frasespeliculas/FraseDTO.java
@@ -0,0 +1,7 @@
+package com.aluracursos.frasespeliculas;
+
+public record FraseDTO(String titulo,
+                       String frase,
+                       String personaje,
+                       String poster ) {
+}
---
+++ frases-peliculas/src/test/java/com/aluracursos/frasespeliculas/FrasesPeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.frasespeliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrasesPeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
